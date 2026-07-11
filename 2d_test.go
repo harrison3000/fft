67bcec7da7011d47bccba02865031bd74cb5d8bb
Harrison <harrison3000@users.noreply.github.com>
2026-07-11 10:45:02 -0300

    more 2d tests

--- 2d_test.go
+++ 2d_test.go
@@ -2,24 +2,51 @@ package fft
 
 import (
 	"fmt"
+	"math/cmplx"
 	"testing"
+
+	dspfft "github.com/mjibson/go-dsp/fft"
 )
 
 func TestFFT2D(t *testing.T) {
-	in := subdivideslice(make([]float32, 16), 4)
-	in[0][0] = 0.5
-	in[1][1] = 0.5
-	in[2][2] = 0.5
-	in[3][3] = 0.5
+	in := mk2dTestData[float32]()
+	in64 := mk2dTestData[float64]()
 
 	s := make([]complex64, 16)
 
-	_, e := Compute2D(in, s)
+	resdsp := dspfft.FFT2Real(in64) //we just trust go-dsp did its homework, lol
+	res, e := Compute2D(in, s)
 	if e != nil {
 		t.Errorf("Well, it should have worked, but didnt (%v)", e)
 	}
 
-	//TODO actually test the return
+	for x := range 8 {
+		for y := range 5 {
+			ours := complex128(res[y][x])
+			theirs := resdsp[x][y]
+
+			abs := cmplx.Abs(ours - theirs)
+
+			if abs > 0.0001 {
+				t.Error("Too much difference")
+			}
+		}
+	}
+}
+
+func mk2dTestData[T float32 | float64]() [][]T {
+	in := subdivideslice(make([]T, 256), 8)
+	in[0][0] = 0.5
+	in[0][1] = 0.5
+	in[1][0] = 0.5
+	in[1][1] = 0.5
+	in[2][2] = 0.5
+	in[3][3] = 0.5
+	in[4][4] = 0.5
+
+	in[5][2] = 0.5
+
+	return in
 }
 
 func TestTranspose(t *testing.T) {
@@ -38,3 +65,5 @@ func TestTranspose(t *testing.T) {
 		t.Errorf("unexpected results: %v", al)
 	}
 }
+
+//TODO benchmarks
